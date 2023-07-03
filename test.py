64653cd6af9a139c402ce1184ef0ded7ce0779dc
DEV <dev@step.rivne>
2023-07-03 16:22:55 +0300

    commid_2

--- test.py
+++ test.py
@@ -1,1 +1,30 @@
-print("Hello world")
+##List
+fruit = ['apple', 'plum', 'banana','orange']
+# rnumbers = list((2,4,5,76,8,3,2))
+
+# # print(fruit)
+# # print(rnumbers)
+# # print(fruit[-1])
+
+# # for i in fruit[0::2]:
+# #     print(i)
+
+# for item in fruit[0::2]:
+#     for s in item:
+#         print(s,end="_")
+#     print()
+
+
+# print('banana' in fruit)
+
+import random
+numbers = [random.randint(20,50) for i in range(10)]
+print(numbers)
+
+letters = [s for s in "banana"]
+print(''.join(letters))
+
+line = 'reverv erverwjwqed wqibdqiw wqibd iwq wqibd'
+
+words = line.split(' ')
+print(words)
